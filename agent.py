3d1f427be4ba56cc3cfe0c92d3d37ebe9fa57ce7
frank1234211 <frank1234211@gmail.com>
2019-01-22 23:50:43 +0800

    fix code and report based on review of Udacity reviewer

--- agent.py
+++ agent.py
@@ -81,7 +81,8 @@ class LearningAgent(Agent):
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
         if self.learning:
-            self.Q[state] = self.Q.get(state, {None:0.0, 'forward':0.0, 'left':0.0, 'right':0.0})
+            if state not in self.Q.keys():
+                self.Q[state] = {None: 0.0, "forward": 0.0, "left": 0.0, "right": 0.0}
 
         return
 
